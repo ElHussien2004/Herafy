a7f43b09b97192ee73b0480f7af845fd757faf2c
Sherien Ayed <Shero@email.com>
2026-03-14 23:13:16 +0200

    Add contant Generic Repo

--- Coer/Domain/Contracts/IGenericRepository.cs
+++ Coer/Domain/Contracts/IGenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Domain.Contracts
 {
-    internal interface IGenericRepository
+    public interface IGenericRepository<TEntity, TKey>where TEntity : BaseEntity<TKey>
     {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity?> GetByIdAsync(TKey id);
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
     }
 }
--- Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,24 @@ using System.Threading.Tasks;
 
 namespace Persistence.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<TEntity, TKey>(HerafyDbContext _dbContext)
+        : IGenericRepository<TEntity, TKey>
+        where TEntity : BaseEntity<TKey>
     {
+        public async Task AddAsync(TEntity entity)
+            => await _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+            => await _dbContext.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+        public void Remove(TEntity entity)
+            => _dbContext.Set<TEntity>().Remove(entity);
+
+        public void Update(TEntity entity)
+            => _dbContext.Set<TEntity>().Update(entity);
     }
 }
