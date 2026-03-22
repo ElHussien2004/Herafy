6f934525b058601de2e3fcf100c759a6eab79e55
ElHussien2004 <abotaleb2322004@gmail.com>
2026-03-22 16:26:13 -0700

    sendOtp

---
+++ Coer/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using ServiceAbstraction;
+using Shared.UserDTOS;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class AuthService(IConnectionMultiplexer connection,ISMSService _SMS) : IAuthService
+    {
+        private readonly IDatabase _database = connection.GetDatabase();
+        public async Task<bool> SendOtpAsync(string phoneNumber)
+        {
+            var otp = new Random().Next(100000, 999999).ToString();
+            var key = $"{phoneNumber}";
+
+            //store in redis
+            await _database.StringSetAsync(key, otp,TimeSpan.FromMinutes(10));
+            var Massage = "TODo";
+           await  _SMS.SendAsync(phoneNumber, Massage);
+           return true;
+        }
+
+        public Task<AuthResultDto> VerifyOtpAsync(string phoneNumber, string otpCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Coer/Service/SMSService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using ServiceAbstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Service
+{
+    public class SMSService(IConfiguration _config) : ISMSService
+    {
+        public async Task SendAsync(string to, string message)
+        {
+            TwilioClient.Init(
+            _config["Twilio:AccountSid"],
+            _config["Twilio:AuthToken"]);
+
+            await MessageResource.CreateAsync(
+                body: message,
+                from: new PhoneNumber(_config["Twilio:PhoneNumber"]),
+                to: new PhoneNumber(to)
+            );
+        }
+    }
+}
