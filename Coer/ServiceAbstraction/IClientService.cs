29ab90e889ae54b8cae6d2d93e641e76ca0030b5
Sherien Ayed <Shero@email.com>
2026-03-28 13:16:16 +0200

    add client service interface

---
+++ Coer/ServiceAbstraction/IClientService.cs
@@ -0,0 +1,19 @@
+﻿using Shared.DTOs.ClientDTOS;
+using Shared.DTOs.TechnicianDTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAbstraction
+{
+   public interface IClientService
+    {
+        Task<IEnumerable<ClientDto>> GetAllAsync();
+        Task<ClientDto?> GetByIdAsync(string id);
+        Task AddAsync(AddClientDto clientDto);
+        Task<bool> UpdateAsync(string id, UpdateClientDto clientDto);
+        Task<bool> DeleteAsync(string id);
+    }
+}
---
+++ Shared/DTOs/ClientDTOS/ClientDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.ClientDTOS
+{
+    public class ClientDto
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string ProfileImageURL { get; set; }
+        public string City { get; set; }
+        public string Government { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
